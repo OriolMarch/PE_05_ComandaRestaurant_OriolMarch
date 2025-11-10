ddeb49517804a73611cc0ee941401c35cb594c64
oriolmarch <icewoo2001@gmail.com>
2025-11-10 16:19:33 +0100

    Crear public void per no posar static

---
+++ PE_05.java
@@ -0,0 +1,10 @@
+public class PE_05{
+    public static void main(String[] args) {
+        PE_05 restaurant = new PE_05();
+        restaurant.principal();
+    }
+    public void principal(){
+    
+    
+    }
+}
